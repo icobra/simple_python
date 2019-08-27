48530b20331ec3d5ac56478f93f6d2b65f8fc1f2
Sasha Good <eiroglif@mail.ru>
2019-08-27 18:43:51 +0300

    Luhn coder/decoder

---
+++ luhn.py
@@ -0,0 +1,106 @@
+#!/usr/bin/python3
+
+""" 
+    Luhn algorithm  - count control 
+
+    Coder/decoder + checking the validity
+"""
+
+def controling(string):
+    # Control str is digit or not. 
+    if str(string).isdigit():
+        return True
+    else:
+        # String must contain in only digit
+        return False
+
+def number_sum(string):
+    # find sum to all digits without ECC number
+    numbers = list(str(string))
+    number_sum = 0
+    lenght = len(numbers)
+    for x in range(0, lenght):
+        if x % 2 == 0:
+            number = (int(numbers[x]) * 2)
+            if int(number) > 9:
+                double_numbers = list(str(number))
+                number = int(double_numbers[0]) + int(double_numbers[1])
+        else:
+            number = (numbers[x])
+        number_sum += int(number)
+    return(number_sum)
+
+def lunh_controling(string):
+    # Count control string.
+    number = str(string)[:-1]
+    total_count = number_sum(number)
+    value = int(total_count) + int(str(string)[-1])
+    # Control multiplicity 10
+    if value % 10 == 0:
+        return True
+    else:
+        return False    
+
+def to_lunh(string):
+    # Add control number.
+    if controling(string):
+        total_count = int(number_sum(string))
+        # Find control number
+        control = 0
+        while control < 10:
+            value = total_count + control  
+            if (value % 10 == 0):
+                break
+            else:
+                control += 1
+        #Make new number
+        lunh_number = str(string) + str(control)
+        return(lunh_number)        
+    else:
+        return("Wrong date type")
+
+def to_normal(string):
+    # Del control number.
+    if lunh_controling(string):
+        string = str(string)[:-1]
+        return string
+    else:
+        return("ECC wrong")
+
+"""
+# Test
+
+#To lunh
+# To normal
+print("to_lunh \n")
+
+print(to_lunh(12345))
+print(to_lunh("123"))
+print(to_lunh("357"))
+print("End to_lunh \n")
+
+
+# To normal
+print("to_normal \n")
+
+print(to_normal(12345))
+print(to_normal("1230"))
+print(to_normal("357"))
+print("End to_normal \n")
+
+# Controling
+print("Controling \n")
+
+print(controling(123))
+print(controling("12345"))
+print(controling("one two three"))
+print("End Controling \n")
+
+# Lunh_controling
+print("Lunch_Controling \n")
+
+print(lunh_controling(32))
+print(lunh_controling(1230))
+print(lunh_controling(234))
+print("End Lunch_Controling \n")
+"""
